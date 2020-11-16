5318d266d1e3a92b0d8f2ebcf10e9ce62084212f
Ashish Sharma <ashish.shm09@gmail.com>
2020-11-17 03:24:14 +0530

    Updated for production

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -11,7 +11,7 @@ class PasswordResetsController < ApplicationController
     @user = User.find_by(email: params[:password_reset][:email].downcase)
     if @user
       @user.create_reset_digest
-      @user.send_password_reset_email
+      
       flash[:info] = "Email sent with password reset instructions"
       redirect_to root_url
     else
@@ -49,17 +49,17 @@ private
 
   # Confirms a valid user.
   def valid_user
-    unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
+    unless (@user && @user.authenticated?(:reset, params[:id]))
       redirect_to root_url
     end
   end
 
-  # Checks expiration of reset token.
-  def check_expiration
-    if @user.password_reset_expired?
-      flash[:danger] = "Password reset has expired."
-      redirect_to new_password_reset_url
-    end
-  end
+  # # Checks expiration of reset token.
+  # def check_expiration
+  #   if @user.password_reset_expired?
+  #     flash[:danger] = "Password reset has expired."
+  #     redirect_to new_password_reset_url
+  #   end
+  # end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,10 +14,10 @@ class User < ApplicationRecord
 
     has_many :followers, through: :passive_relationships, source: :follower                                    
     
-    attr_accessor :remember_token, :activation_token, :reset_token
+    attr_accessor :remember_token
 
     before_save :downcase_email
-    before_create :create_activation_digest
+    # before_create :create_activation_digest
     
     validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -64,31 +64,31 @@ class User < ApplicationRecord
         BCrypt::Password.new(digest).is_password?(token)
     end
 
-    # Activates an account.
-    def activate
-        update_columns(activated: true, activated_at: Time.zone.now)
-    end
+    # # Activates an account.
+    # def activate
+    #     update_columns(activated: true, activated_at: Time.zone.now)
+    # end
     
-    # Sends activation email.
-    def send_activation_email
-        UserMailer.account_activation(self).deliver_now
-    end
+    # # Sends activation email.
+    # def send_activation_email
+    #     UserMailer.account_activation(self).deliver_now
+    # end
 
     # Sets the password reset attributes.
-    def create_reset_digest
-        self.reset_token = User.new_token
-        update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
-    end
+    # def create_reset_digest
+    #     self.reset_token = User.new_token
+    #     update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+    # end
 
-    # Sends password reset email.
-    def send_password_reset_email
-        UserMailer.password_reset(self).deliver_now
-    end
+    # # Sends password reset email.
+    # def send_password_reset_email
+    #     UserMailer.password_reset(self).deliver_now
+    # end
 
-    # Returns true if a password reset has expired.
-    def password_reset_expired?
-        reset_sent_at < 2.hours.ago #earlier than 2 hours
-    end
+    # # Returns true if a password reset has expired.
+    # def password_reset_expired?
+    #     reset_sent_at < 2.hours.ago #earlier than 2 hours
+    # end
 
     # Forgets a user.
     def forget
@@ -117,9 +117,9 @@ class User < ApplicationRecord
     end
 
     # Creates and assigns the activation token and digest.
-    def create_activation_digest
-        self.activation_token = User.new_token
-        self.activation_digest = User.digest(activation_token)
-    end
+    # def create_activation_digest
+    #     self.activation_token = User.new_token
+    #     self.activation_digest = User.digest(activation_token)
+    # end
 end
     
